0f11effca677823fd3439ddb8b871e686f3fde04
David Guess <david.guess@gmail.com>
2021-07-08 12:15:05 +1000

    clean up id

--- src/components/ProjectsGrid.vue
+++ src/components/ProjectsGrid.vue
@@ -7,7 +7,6 @@
                 :alt="item.node.title"
                 class="thumbnail"
             />
-            <h3>{{ item.node.id }}</h3>
             <h3 class="project-title">{{ item.node.title }}</h3>
             </g-link>
         </div>
